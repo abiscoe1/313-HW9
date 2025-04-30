fc7172c44a3d17ef7d8bc4a5ee99c4852d9d19a2
abiscoe1 <abiscoe1@umbc.edu>
2025-04-30 14:42:56 -0400

    Update myProgram.asm

--- myProgram.asm
+++ myProgram.asm
@@ -4,9 +4,9 @@ SECTION .data
 msg     db      'Hello World!', 0Ah ;Create a msg variable to hold the string 
 	
 SECTION .text
-	global  _start 		;Create a label _start to indicate the start of the instructions to				    be executed
+	global  _start 		;Create a label _start to indicate the start of the instruction to be executed
 _start:	
-	mov     edx, 13		;Allocate 13 bytes for the 12 characters in the string, including				    the space, and an extra byte for the line feed character
+	mov     edx, 13		;Allocate 13 bytes for the 12 characters in the string, including the space, and an extra byte for the line feed character
 	mov     ecx, msg	;Load the address of the msg variable in ecx
 	mov     ebx, 1		;Move the string to the STDOUT file so it can be printed
 	mov     eax, 4		;Print the string
